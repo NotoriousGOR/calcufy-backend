d26faa5627a31174452d4b9c317ae7f4f95667ac
Gabriel Rosales <gabriel.rosales0129@gmail.com>
2022-11-02 19:54:42 -0500

    working on things

--- handlers/status.js
+++ handlers/status.js
@@ -1,7 +1,7 @@
 exports.handler = async (event, context, callback) => {
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ up: true })
-  }
-}
+	return {
+		statusCode: 200,
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({ up: true }),
+	};
+};
