2de1d9bcaba99104b875dc7214a4ee296f66f47a
Gabriel Rosales <gabriel.rosales0129@gmail.com>
2022-11-02 20:44:37 -0500

    working on operation costs

--- handlers/operations.js
+++ handlers/operations.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-exports.handler = async (event) => {
+exports.handler = async (event, context, callback) => {
 	try {
 		const { Authorization: token } = event.headers["Authorization"];
 		// checks for username, password, and that the password is at least 8 characters
@@ -36,6 +36,46 @@ exports.handler = async (event) => {
 			};
 		}
 
+		if (
+			event.a === undefined ||
+      event.b === undefined ||
+      event.op === undefined
+		) {
+			callback("400 Invalid Input");
+		}
+
+		var res = {};
+		res.a = Number(event.a);
+		res.b = Number(event.b);
+		res.op = event.op;
+
+		if (isNaN(event.a) || isNaN(event.b)) {
+			callback("400 Invalid Operand");
+		}
+
+		switch (event.op) {
+		case "+":
+		case "add":
+			res.c = res.a + res.b;
+			break;
+		case "-":
+		case "sub":
+			res.c = res.a - res.b;
+			break;
+		case "*":
+		case "mul":
+			res.c = res.a * res.b;
+			break;
+		case "/":
+		case "div":
+			res.c = res.b === 0 ? NaN : Number(event.a) / Number(event.b);
+			break;
+		default:
+			callback("400 Invalid Operator");
+			break;
+		}
+		callback(null, res);
+
 		return {
 			statusCode: 200,
 			headers: { "Content-Type": "application/json" },
@@ -55,3 +95,5 @@ exports.handler = async (event) => {
 		};
 	}
 };
+
+, , , , )
